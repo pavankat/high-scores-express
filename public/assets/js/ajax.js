47537075ae2dc1f4274a3c45d8918cbe3f918198
Pavan Katepalli <pavankatepalli@Pavans-MBP-2.home>
2017-03-14 20:31:39 -0400

    rps game working

--- public/assets/js/ajax.js
+++ public/assets/js/ajax.js
@@ -1,19 +1,54 @@
-$('#submitScore').on('click', function(e){
-	var randNum = Math.random(); //this gets me random number between 0 and 1
+var computerChoices = ["r", "p", "s"];
 
-	var randNumUpToFiveHund = randNum * 500;
+function computerChooses(arr){
+	var randomComputerChoiceIndex = Math.floor(Math.random() * arr.length);
 
-	var roundUpRand = Math.floor(randNumUpToFiveHund);
+	return arr[randomComputerChoiceIndex];
+}
 
-	var data = {
-		total_score: roundUpRand,
+var score = 0;
+var turns = 15;
+
+$('#score').text(score);
+$('#turns').text(turns);
+
+$('.choice').on('click', function(){
+	var randomComputerChoice = computerChooses(computerChoices);
+
+	var yourChoice = $(this).data('choice');
+
+	if (yourChoice == randomComputerChoice){
+		score = score + 10;
+		$('#status').text('you tied!');
+	}else if(yourChoice == 'r' && randomComputerChoice == 's'){
+		score = score + 30;
+		$('#status').text('you won!');
+	}else if(yourChoice == 'p' && randomComputerChoice == 'r'){
+		score = score + 30;
+		$('#status').text('you won!');
+	}else if(yourChoice == 's' && randomComputerChoice == 'p'){
+		score = score + 30;
+		$('#status').text('you won!');
+	}else{
+		score = score - 30;
+		$('#status').text("You lost! You're a loser!")
 	}
 
-	$.ajax({
-		url: "/scores/create", 
-		method: "POST",
-		data: data, 
-	}).done(function(response){
-		window.location = "/scores"
-	});
+	turns--;
+	$('#turns').text(turns);
+	$('#score').text(score);
+
+	if (turns == 0){
+		var data = {
+			total_score: score,
+		}
+
+		$.ajax({
+			url: "/scores/create", 
+			method: "POST",
+			data: data, 
+		}).done(function(response){
+			window.location = "/scores"
+		});
+	}
 });
